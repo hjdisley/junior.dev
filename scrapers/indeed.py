9e457206f698ba6708ecd128774e56de848cbc41
Hayden Disley <hayden@pc-6-209.customer.ask4.lan>
2022-04-02 17:01:39 +0100

    changed indeed scraper

--- scrapers/indeed.py
+++ scrapers/indeed.py
@@ -7,47 +7,64 @@ def get_data(url):
     r = requests.get(url)
     return r.content
 
-def get_page_data_and_desc(page):
+def get_long_description(individual_job_page):
+    pass
+
+def scrape_indeed(page):
     indeed = get_data(f"https://uk.indeed.com/jobs?q=junior%20developer&l=united%20kingdom&fromage=7start={page}")
 
     indeed_soup = BeautifulSoup(indeed, "html.parser")
 
-    try:
-        job_desc = indeed_soup.find(id="jobDescriptionText")
-        job_desc = job_desc.text.replace('\n', ' ').replace('\r', '')
-    except:
-        job_desc = "No Description Specified"
+    indeed_results = indeed_soup.find(id='resultsCol')
 
-    return indeed_soup, job_desc
+    indeed_jobs = indeed_results.find_all('div', class_='job_seen_beacon')
 
-def scrape_job_result(job_data):
-    try:
-        job_title = job_data.find("h2", class_="jobTitle").get_text()
-    except:
-        job_title = "No Job Title Specified"
-    
-    try:
-        job_company = job_data.find("span", class_="companyName").get_text()
-    except:
-        job_company = "No Company Specified"
+    job_data = []
+    for job in indeed_jobs:
+            
 
-    try:
-        job_location = job_data.find("div", class_="companyLocation")
-    except:
-        job_location = "No Location Specified"
-    
-    try:
-        job_salary = job_data.find("div", class_="metadata salary-snippet-container").get_text()
-    except:
-        job_salary = "Not Specified"
-    
-    return job_title, job_company, job_location, job_salary
+        try:
+            job_salary = job.find('div', class_='salary-snippet').text
+        except:
+            job_salary = 'No Salary Specified'
 
-def scrape_full_job_page(page_html):
-    pass
+        try:
+            if job_title.text[0:3] == 'new':
+                job_title = job_title.text[3:len(job_title.text)]
+            else:
+                job_title = job_title.text
+        except: 
+            job_title = 'No Title Specified'
 
-def scrape_indeed():
-    pass    
+        try:
+            job_company = job.find('span', class_='companyName').text 
+
+        except:
+            job_company = 'No Company Specified'
+        
+        try:
+            job_location = job.find('div', class_='companyLocation').text
+
+        except:
+            job_location = 'No Location Specified'
+
+        try:
+            job_desc = job.find('div', class_='job-snippet').text
+        except:
+            job_desc = 'No Description Specified'
+
+        job_data.append({
+            "title": job_title,
+            "company": job_company,
+            "location": job_location,
+            "salary": job_salary,
+            "description": job_desc
+        })
+
+        print(job_data)
+
+scrape_indeed(10)
+
 
 
 
