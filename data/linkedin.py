038dead70c0dd49b23ec9b488a76323681fb9c20
hjdisley <hjdisley@gmail.com>
2022-04-26 22:21:34 +0100

    Solved error of nonetype is not subscriptable for links

--- data/linkedin.py
+++ data/linkedin.py
@@ -42,14 +42,54 @@ def login_linkedin():
         sleep(3)
         print('Successfully Logged In...')
 
-        scrape_linkedin('https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=101165590&keywords=junior%20software%20engineer&location=United%20Kingdom')
-
-        driver.quit()
     except Exception as e: 
         print('Error logging in ->', e)
 
+    scrape_linkedin('https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=101165590&keywords=junior%20software%20engineer&location=United%20Kingdom')
+
+    driver.quit()
+
 def scrape_job_page(job_page_url):
-    print(job_page_url)
+    individual_job_page = get_data(job_page_url)
+
+    individual_job_page_soup = BeautifulSoup(individual_job_page, 'html.parser')
+
+    job_data = []
+    try:
+        job_title = individual_job_page_soup.find('h1').get_text()
+    except:
+        job_title = 'No Title Specified'
+
+    try: 
+        job_salary = individual_job_page_soup.find('a', {'class': 'app-aware-link'}).get_text()
+    except:
+        job_salary = 'No Salary Specified'
+    
+    try: 
+        job_location = individual_job_page_soup.find('span', {'class', 'jobs-unified-top-card__bullet'}).get_text()
+    except:
+        job_location = 'No Location Specified'
+
+    try:
+        job_workplace_type = individual_job_page_soup.find('span', {'class', 'jobs-unified-top-card__workplace-type'}).get_text()
+    except:
+        job_workplace_type = 'No Workplace Type Specified'
+    
+    try:
+        job_description_long = individual_job_page_soup.find('div', {'class': 'jobs-box__html-content'}).find_all('p')
+    except:
+        job_description_long = 'No Long Description Specified'
+    
+    job_data.append({
+        'job_title': job_title,
+        'job_salary': job_salary,
+        'job_location': job_location,
+        'job_workplace_type': job_workplace_type,
+        'job_description_long': job_description_long
+    })
+
+    print(job_data)
+    
 
 def scrape_linkedin(url):
     linkedin = get_data(url)
@@ -59,11 +99,12 @@ def scrape_linkedin(url):
    
     job_urls = []
     for job in linkedin_results: 
-        individual_job_url = job.find('a', {'class': 'base-card__full-link'})['href']
+        individual_job_url = job.find('a', 'base-card__full-link', href=True)['href']
+
         job_urls.append(individual_job_url)
 
-    
-    for url in job_urls:
-        scrape_job_page(url)
+    for index, url in enumerate(job_urls):
+        if index == 0:
+            scrape_job_page(url)
         
 login_linkedin()
